07e12e03830c8e9504fe946689907d2b9c6315f9
nandishk62 <nandishku902@gmail.com>
2024-10-18 12:02:34 +0530

    update

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -9,12 +9,14 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
   const searchParams=useSearchParams();
   const pathname = usePathname();
+
   const { replace } = useRouter();
 
   function handleSearch(term: string) {
     const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
     const params=new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term){
       params.set('query',term);
     }else{
