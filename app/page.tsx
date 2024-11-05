698c6cf5160f65d7f38447e50a4c637a2f22e521
nandishk62 <nandishku902@gmail.com>
2024-11-05 15:50:17 +0530

    Updated

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default function Page() {
             href="/dashboard"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
-            <span>dashboard</span> <ArrowRightIcon className="w-5 md:w-6" />
+            <span>dashboard !</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
           <Link
             href="/login"
