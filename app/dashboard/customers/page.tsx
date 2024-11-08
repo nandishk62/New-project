1b930ec0301638cc86fd129d43df22aa59982fb0
nandishk62 <nandishku902@gmail.com>
2024-11-08 17:34:54 +0530

    update

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
     return (
     
     <div className="flex w-full items-center justify-between bg-red">
-      <h1 className={`${lusitana.className} text-2xl bg-green`}>Customers</h1>
+      <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
       
     </div>
     
