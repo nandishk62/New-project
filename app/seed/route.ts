a30a73bdfcc411fe9c707f6e4aca6e83778a81c5
nandishk62 <nandishku902@gmail.com>
2024-11-15 12:05:19 +0530

    update

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -100,7 +100,7 @@
 //   return insertedRevenue;
 // }
 
- export async function GET() {
+  export async function GET() {
     return Response.json({
     message:
       'Uncomment this file and remove this line. You can delete this file when you are finished.',
